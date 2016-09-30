8f113f02bd5bf0164b582723e304c98e05215db6
Giordan Arevalo <gbap0506@hotmail.com>
2016-09-29 22:22:25 -0500

    incidencias

--- resources/views/admin/template/footer.blade.php
+++ resources/views/admin/template/footer.blade.php
@@ -2,8 +2,8 @@
 	<div class="footer-inner">
 		<div class="footer-content">
 			<span class="bigger-120">
-				<span class="blue bolder">Admin</span>
-				Incidencias &copy; 2016
+				<span class="blue bolder">Sistema para el control de atención de clientes</span>
+				
 			</span>
 		</div>
 	</div>
